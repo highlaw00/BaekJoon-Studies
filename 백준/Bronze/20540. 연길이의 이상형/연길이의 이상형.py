97022afd219c934db50fc8a504445619c6d5972f
highlaw00 <65754646+highlaw00@users.noreply.github.com>
2022-09-11 11:59:55 +0900

    [Bronze III] Title: 연길이의 이상형, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/20540. 연길이의 이상형/연길이의 이상형.py
@@ -0,0 +1,21 @@
+s = input()
+
+if s[0] == 'E':
+    print('I', end = '')
+else:
+    print('E', end = '')
+
+if s[1] == 'N':
+    print('S', end = '')
+else:
+    print('N', end = '')
+
+if s[2] == 'T':
+    print('F', end = '')
+else:
+    print('T', end = '')
+
+if s[3] == 'J':
+    print('P', end = '')
+else:
+    print('J', end = '')
