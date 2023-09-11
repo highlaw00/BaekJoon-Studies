d7e46678820b3f617e8d1acd6102239637020f43
highlaw00 <65754646+highlaw00@users.noreply.github.com>
2023-09-12 02:05:30 +0900

    [Bronze IV] Title: 주사위 세개, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,15 @@
+a, b, c = map(int, input().split())
+ans = 0
+if a == b == c:
+    ans = 10000 + a * 1000
+elif a == b or b == c or a == c:
+    if a == b:
+        ans = 1000 + a * 100
+    elif b == c:
+        ans = 1000 + b * 100
+    else:
+        ans = 1000 + c * 100
+else:
+    ans = max(a, b, c) * 100
+
+print(ans)
