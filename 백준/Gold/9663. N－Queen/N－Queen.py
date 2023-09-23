19b38e55c54931205c64a2d942ed0d85ed67e88f
highlaw00 <65754646+highlaw00@users.noreply.github.com>
2023-09-23 14:50:47 +0900

    [Gold IV] Title: N-Queen, Time: 9792 ms, Memory: 139300 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -0,0 +1,62 @@
+import time
+
+start = time.time()
+n = int(input())
+grid = [[0 for _ in range(n)] for _ in range(n)]
+availability_grid = [[0 for _ in range(n)] for _ in range(n)]
+ans = 0
+
+
+def color(r, c):
+    # c열을 모두 색칠합니다.
+    # r,c의 대각방향의 보드에는 모두 색칠합니다.
+    availability_grid[r][c] += 1
+    # 수직 색칠
+    for i in range(r+1, n):
+        availability_grid[i][c] += 1
+    # 대각(좌측) 색칠
+    acc = 1
+    while r + acc < n and c - acc >= 0:
+        availability_grid[r+acc][c-acc] += 1
+        acc += 1
+    # 대각(우측) 색칠
+    acc = 1
+    while r + acc < n and c + acc < n:
+        availability_grid[r+acc][c+acc] += 1
+        acc += 1
+
+
+def erase(r, c):
+    # c열을 모두 지웁니다.
+    # r,c의 대각방향의 보드를 모두 지웁니다.
+    availability_grid[r][c] -= 1
+    # 수직 색칠
+    for i in range(r+1, n):
+        availability_grid[i][c] -= 1
+    # 대각(좌측) 색칠
+    acc = 1
+    while r + acc < n and c - acc >= 0:
+        availability_grid[r+acc][c-acc] -= 1
+        acc += 1
+    # 대각(우측) 색칠
+    acc = 1
+    while r + acc < n and c + acc < n:
+        availability_grid[r+acc][c+acc] -= 1
+        acc += 1
+
+
+def back(cnt):
+    if cnt == n:
+        global ans
+        ans += 1
+        return
+    for i in range(n):
+        # 가능한 경우
+        if availability_grid[cnt][i] == 0:
+            color(cnt, i)
+            back(cnt + 1)
+            erase(cnt, i)
+
+
+back(0)
+print(ans)
