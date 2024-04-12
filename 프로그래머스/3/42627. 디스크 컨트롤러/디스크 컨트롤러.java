b617ab4ad026654ea6bf6ed464f67a265fd817d1
highlaw00 <65754646+highlaw00@users.noreply.github.com>
2024-04-12 12:03:28 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 1.54 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -2,62 +2,40 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] jobs) {
-        PriorityQueue<ArrayList<Integer>> pq = new PriorityQueue<>(startTimeAscending);
-        for (int i=0; i<jobs.length; i++) {
-            ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(jobs[i][0], jobs[i][1]));
-            pq.add(arr);
+        // 시작 순서를 기준으로 오름차순
+        Arrays.sort(jobs, (o1, o2) -> Integer.compare(o1[0], o2[0]));
+
+        // 소요 시간 기준으로 오름차순
+        PriorityQueue<int[]> executablePriorityQueue = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[1], o2[1]));
+
+        int waitTime = 0; // 모든 job들의 대기 시간
+        int endTime = jobs[0][0]; // 가장 최근의 job이 끝난 시간
+        int jobIndex = 0;
+        int executedCnt = 0; // 수행된 job 카운터
+
+        while (executedCnt < jobs.length) {
+            // endTime보다 같거나 작은 모든 job을 선택
+            while (jobIndex < jobs.length && jobs[jobIndex][0] <= endTime) {
+                executablePriorityQueue.add(jobs[jobIndex]);
+                jobIndex ++;
+            }
+
+            // 만약 아무것도 선택된 job이 없으면 현재 endTime보다 작은 startTime을 가지는 job이 없는것
+            if (executablePriorityQueue.isEmpty()) {
+                endTime = jobs[jobIndex][0];
+                continue;
+            }
+
+            // 수행 가능한 job 중 가장 수행시간이 짧은것을 뽑아 수행
+            int[] job = executablePriorityQueue.poll();
+            int arrivedTime = job[0];
+            int elapseTime = job[1];
+            waitTime += endTime - arrivedTime;
+            waitTime += elapseTime;
+            endTime += elapseTime;
+            executedCnt += 1;
         }
-        int curr = pq.peek().get(0); // 현재 시각 최초의 job이 수행 시작 시간
-        int answer = 0;
-        while (!pq.isEmpty()) {
-            PriorityQueue<ArrayList<Integer>> executable = new PriorityQueue<>(elapseTimeAscending);
-            // 현재 수행할 수 있는 job을 여러개 뽑는다.
-            while (!pq.isEmpty() && pq.peek().get(0) <= curr) {
-                executable.add(pq.poll());
-            }
-            
-            if (executable.isEmpty()) executable.add(pq.poll());
 
-            // 수행할 수 있는 job 중 수행 시간이 가장 짧은 job을 선택한다.
-            ArrayList<Integer> pair = executable.poll();
-            int arrivedTime = pair.get(0);
-            int elapseTime = pair.get(1);
-            if (arrivedTime > curr) {
-                curr = arrivedTime;
-            }
-            answer += (curr - arrivedTime); // 대기 시간 누적
-            answer += elapseTime; // 소요 시간 누적
-            curr += elapseTime; // 현재 시간 갱신
-
-            while (!executable.isEmpty()) {
-                pq.add(executable.poll());
-            }
-        }
-
-        return answer / jobs.length;
+        return waitTime / jobs.length;
     }
-
-    public Comparator<ArrayList<Integer>> startTimeAscending = new Comparator<>() {
-        @Override
-        public int compare(ArrayList<Integer> o1, ArrayList<Integer> o2) {
-            if (o1.get(0) > o2.get(0)) return 1;
-            else if (o1.get(0) == o2.get(0)) {
-                if (o1.get(1) > o2.get(1)) return 1;
-                else if (o1.get(1) == o2.get(1)) return 0;
-                else return -1;
-            } else return -1;
-        }
-    };
-
-    public Comparator<ArrayList<Integer>> elapseTimeAscending = new Comparator<>() {
-        @Override
-        public int compare(ArrayList<Integer> o1, ArrayList<Integer> o2) {
-            if (o1.get(1) > o2.get(1)) return 1;
-            else if (o1.get(1) == o2.get(1)) {
-                if (o1.get(0) > o2.get(0)) return 1;
-                else if (o1.get(0) == o2.get(0)) return 0;
-                else return -1;
-            } else return -1;
-        }
-    };
 }
