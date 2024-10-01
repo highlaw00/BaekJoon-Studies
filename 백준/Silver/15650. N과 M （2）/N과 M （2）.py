d94f4721dadfc70438f9f3892f5af8c7d5a12018
highlaw00 <65754646+highlaw00@users.noreply.github.com>
2024-10-01 14:51:36 +0900

    [Silver III] Title: N과 M (2), Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,25 +1,15 @@
-n, m = map(int, input().split())
-
-ans = [0 for _ in range(m)]
-check = [False for _ in range(n)]
-
-
-def back(cnt):
-    # 기저 사례
+def back(cnt, k, stack, arr, m):
     if cnt == m:
-        for num in ans:
-            print(num, end=' ')
-        print()
+        print(' '.join(map(str, stack)))
         return
-    for i in range(n):
-        if not check[i]:
-            if cnt != 0 and i+1 < ans[cnt-1]:
-                continue
-            # i번째 숫자가 ans에 없다면
-            check[i] = True
-            ans[cnt] = i+1
-            back(cnt + 1)
-            check[i] = False
+    for i in range(k, len(arr)):
+        num = arr[i]
+        if num in stack: 
+            continue
+        stack.append(num)
+        back(cnt+1, i+1, stack, arr, m)
+        stack.pop()
 
-
-back(0)
+n, m = map(int, input().split())
+arr = [i for i in range(1, n+1)]
+back(0, 0, [], arr, m)
