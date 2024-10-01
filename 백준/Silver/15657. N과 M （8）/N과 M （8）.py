752fee7c9cd9de0aaeeed9243a8ff83c3299522f
highlaw00 <65754646+highlaw00@users.noreply.github.com>
2024-10-01 18:32:02 +0900

    [Silver III] Title: N과 M (8), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15657. N과 M （8）/N과 M （8）.py
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -1,29 +1,15 @@
-# 길이가 m인 수열을 모두 구하라, n개의 자연수는 모두 다른 수
-# n개의 자연수 중 m개를 고른 수열
-# 같은 수를 여러번 골라도 됨
-# 고른 수열은 비내림차순이어야 함
-# 비내림차순이란 a1 <= a2 <= ... <= ak-1 <= ak를 만족하면 비내림차순
+n, m = map(int,input().split())
+numbers = sorted(list(map(int, input().split())))
+s = []
 
-# 오름차순으로 리스트를 정렬한 뒤 백트래킹 하면 될 듯
-
-n, m = map(int, input().split())
-
-A = list(map(int, input().split()))
-A.sort()
-sheet = []
-
-
-def back(cnt, idx):
-    # 기저 사례 삽입
-    if cnt == m:
-        print(' '.join(map(str, sheet)))
+def back(k):
+    if len(s) == m:
+        print(' '.join(map(str, s)))
         return
+    for i in range(k, len(numbers)):
+        num = numbers[i]
+        s.append(num)
+        back(i)
+        s.pop()
 
-    # 하나씩 뽑기
-    for i in range(idx, n):
-        sheet.append(A[i])
-        back(cnt+1, i)
-        sheet.pop()
-
-
-back(0, 0)
+back(0)
