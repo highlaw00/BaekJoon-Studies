285679a7757537997333ea9f5b2f035ace198d99
highlaw00 <65754646+highlaw00@users.noreply.github.com>
2024-10-01 18:13:12 +0900

    [Silver III] Title: N과 M (6), Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -0,0 +1,15 @@
+n, m = map(int, input().split())
+numbers = sorted(list(map(int, input().split())))
+s = []
+
+def back(cnt, k):
+    if cnt == m:
+        print(' '.join(map(str, s)))
+        return
+    for i in range(k, len(numbers)):
+        num = numbers[i]
+        s.append(num)
+        back(cnt+1, i+1)
+        s.pop()
+
+back(0, 0)
